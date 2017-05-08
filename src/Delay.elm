72acd29552f29855ddbd6e4ba49e0d6ded1d4a67
Andrew MacMurray <a.macmurray@icloud.com>
2017-05-08 21:55:52 +0100

    refactor Delay.start to return a Cmd, remove opaque State type

--- src/Delay.elm
+++ src/Delay.elm
@@ -1,7 +1,6 @@
 module Delay
     exposing
         ( after
-        , State
         , start
         , handleSequence
         )
@@ -12,18 +11,13 @@ module Delay
 @docs after
 
 # Delay a sequence of messages
-@docs State, start, handleSequence
+@docs start, handleSequence
 
 -}
 
 import Process
 import Task
 import Time exposing (millisecond)
-
-
-type Phase
-    = First
-    | Rest
 
 
 {-| Delays an update (with a message) by a given number of milliseconds
@@ -35,71 +29,51 @@ after ms msg =
         |> Task.perform identity
 
 
-{-| Opaque type to keep track of the state of a sequence
--}
-type State msg
-    = State Phase (List ( Float, msg ))
-
-
 {-| Starts the sequence of messages
 -}
-start : (State msg -> msg) -> List ( Float, msg ) -> msg
-start msg msgs =
-    msg (State First msgs)
+start : (List ( Float, msg ) -> msg) -> List ( Float, msg ) -> Cmd msg
+start sequenceMsg msgs =
+    let
+        firstDelay =
+            getDelay 0 msgs
+    in
+        after firstDelay (sequenceMsg msgs)
 
 
 {-| Calls update with each message and a delay until finished
 -}
-handleSequence : (State msg -> msg) -> State msg -> (msg -> model -> ( model, Cmd msg )) -> model -> ( model, Cmd msg )
-handleSequence sequenceMsg (State phase msgs) update model =
-    case phase of
-        First ->
-            case List.head msgs of
-                Just ( ms, _ ) ->
-                    model ! [ sequenceDelay ms (State Rest msgs) sequenceMsg ]
+handleSequence : (List ( Float, msg ) -> msg) -> List ( Float, msg ) -> (msg -> model -> ( model, Cmd msg )) -> model -> ( model, Cmd msg )
+handleSequence sequenceMsg msgs update model =
+    case List.head msgs of
+        Just ( _, msg ) ->
+            let
+                remainingMsgs =
+                    List.tail msgs |> Maybe.withDefault []
 
-                Nothing ->
-                    model ! []
+                nextDelay =
+                    (getDelay 1) msgs
 
-        Rest ->
-            case List.head msgs of
-                Just ( _, msg ) ->
-                    let
-                        remainingMsgs =
-                            List.tail msgs |> Maybe.withDefault []
+                ( newModel, cmd ) =
+                    update msg model
 
-                        nextDelay =
-                            nextDelayDuration msgs |> Maybe.withDefault 0
+                nextCmd =
+                    if List.length msgs > 1 then
+                        after nextDelay (sequenceMsg remainingMsgs)
+                    else
+                        Cmd.none
+            in
+                newModel ! [ cmd, nextCmd ]
 
-                        ( newModel, cmd ) =
-                            update msg model
-
-                        nextCmd =
-                            if List.length msgs > 1 then
-                                sequenceDelay nextDelay (State Rest remainingMsgs) sequenceMsg
-                            else
-                                Cmd.none
-                    in
-                        newModel ! [ cmd, nextCmd ]
-
-                Nothing ->
-                    model ! []
+        Nothing ->
+            model ! []
 
 
-{-| Private. Hands the next piece of sequence state to the update function after a delay
+{-| Private, gets a delay value by index from a list of (delay, msg)
 -}
-sequenceDelay : Float -> State msg -> (State msg -> msg) -> Cmd msg
-sequenceDelay ms (State phase msgs) sequenceMsg =
-    Process.sleep (millisecond * ms)
-        |> Task.map (always (sequenceMsg (State phase msgs)))
-        |> Task.perform identity
-
-
-{-| Private. Gets the next delay duration of the update sequence
--}
-nextDelayDuration : List ( Float, msg ) -> Maybe Float
-nextDelayDuration msgs =
+getDelay : Int -> List ( Float, msg ) -> Float
+getDelay n msgs =
     msgs
-        |> List.drop 1
+        |> List.drop n
         |> List.head
         |> Maybe.map Tuple.first
+        |> Maybe.withDefault 0
